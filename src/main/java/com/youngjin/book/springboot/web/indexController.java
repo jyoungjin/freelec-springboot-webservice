d6705017b0516efc8bc01b6c0e4d3976f99b84a3
jyoungjin <ccd1202@naver.com>
2022-04-12 20:03:09 +0900

    Chapter05. 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기

--- src/main/java/com/youngjin/book/springboot/web/indexController.java
+++ src/main/java/com/youngjin/book/springboot/web/indexController.java
@@ -1,24 +1,31 @@
 package com.youngjin.book.springboot.web;
 
+import com.youngjin.book.springboot.config.auth.LoginUser;
+import com.youngjin.book.springboot.config.auth.dto.SessionUser;
 import com.youngjin.book.springboot.service.posts.PostsService;
 import com.youngjin.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class indexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
-
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
